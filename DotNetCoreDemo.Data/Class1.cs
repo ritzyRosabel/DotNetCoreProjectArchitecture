b8a9c78921588f0c8a4328693885104b195ffa7c
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-14 00:39:04 +0100

    included Data and Services projects

---
+++ DotNetCoreDemo.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DotNetCoreDemo.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ DotNetCoreDemo.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DotNetCoreDemo.Services
+{
+    public class Class1
+    {
+    }
+}
